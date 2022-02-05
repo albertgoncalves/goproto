55439aa656b5f734af58a4457dbe0fc9d304304a
albertgoncalves <goncalves.albert@gmail.com>
2022-02-05 00:34:42 -0500

    concurrency demo

---
+++ src/sleep.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+    "fmt"
+    "sync"
+    "time"
+    "math/rand"
+)
+
+func f(wg *sync.WaitGroup, j int) {
+    defer wg.Done()
+    t := time.Duration(rand.Int31n(2500)) * time.Millisecond
+    fmt.Printf("  [ %2d ] sleeping for %v\n", j, t)
+    time.Sleep(t)
+    fmt.Printf("  [ %2d ] done\n", j)
+}
+
+func main() {
+    var wg sync.WaitGroup
+    for i := 1; i <= 10; i++ {
+        wg.Add(1)
+        go f(&wg, i)
+    }
+    wg.Wait()
+}
