661e392af679d8242741b0c6bdd414aa080b2df8
albertgoncalves <goncalves.albert@gmail.com>
2025-08-01 17:02:41 -0400

    clean-up implementation

--- src/quickselect.go
+++ src/quickselect.go
@@ -1,73 +1,68 @@
 package main
 
 import (
+    "cmp"
     "fmt"
     "math/rand"
     "time"
 )
 
-type t = uint8
+func partition[T cmp.Ordered](array []T, l, r, i int) int {
+    x := array[i]
 
-func partition(array []t, left, right, pivotIndex int) int {
-    pivotValue := array[pivotIndex]
-    array[pivotIndex], array[right] = array[right], array[pivotIndex]
-    storeIndex := left
-    for i := left; i < right; i++ {
-        if array[i] < pivotValue {
-            array[storeIndex], array[i] = array[i], array[storeIndex]
-            storeIndex++
+    array[i], array[r] = array[r], array[i]
+
+    j := l
+    for i := l; i < r; i++ {
+        if array[i] < x {
+            array[j], array[i] = array[i], array[j]
+            j++
         }
     }
-    array[storeIndex], array[right] = array[right], array[storeIndex]
-    return storeIndex
+
+    array[j], array[r] = array[r], array[j]
+
+    return j
 }
 
-func quickSelect(r *rand.Rand, array []t, left, right, k int) t {
+func quickSelect[T cmp.Ordered](rng *rand.Rand, array []T, l, r, k int) T {
     for {
-        if left == right {
-            return array[left]
+        if l == r {
+            return array[l]
         }
-        pivotIndex := partition(array, left, right, left+r.Intn(right-left+1))
-        if k == pivotIndex {
+
+        i := partition(array, l, r, l+rng.Intn(r-l+1))
+
+        if k == i {
             return array[k]
-        } else if k < pivotIndex {
-            right = pivotIndex - 1
+        } else if k < i {
+            r = i - 1
         } else {
-            left = pivotIndex + 1
+            l = i + 1
         }
     }
 }
 
-func quickSort(r *rand.Rand, array []t, left, right int) {
-    if left < right {
-        pivotIndex := partition(
-            array,
-            left,
-            right,
-            left+r.Intn(right-left+1),
-        )
-        quickSort(r, array, left, pivotIndex)
-        quickSort(r, array, pivotIndex+1, right)
+func quickSort[T cmp.Ordered](rng *rand.Rand, array []T, l, r int) {
+    if r <= l {
+        return
     }
+
+    i := partition(array, l, r, l+rng.Intn(r-l+1))
+    quickSort(rng, array, l, i)
+    quickSort(rng, array, i+1, r)
 }
 
 func main() {
-    array := []t{3, 4, 2, 5, 7, 1, 8, 2, 9, 10}
-    n := len(array)
-    r := rand.New(rand.NewSource(time.Now().UnixNano()))
-    fmt.Println("\noriginal array   :", array)
-    {
-        for i := 0; i < n; i++ {
-            a := make([]t, len(array))
-            copy(a, array)
-            v := quickSelect(r, a, 0, n-1, i)
-            fmt.Println("post-quickselect :", a, "| k :", i, "| value :", v)
-        }
+    rng := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+    for i := 0; i < 10; i++ {
+        array := []int{3, 4, 2, 5, 7, 1, 8, 2, 9, 10}
+        v := quickSelect(rng, array, 0, len(array)-1, i)
+        fmt.Println("post-quickselect :", array, "| k :", i, "| value :", v)
     }
-    {
-        a := make([]t, len(array))
-        copy(a, array)
-        quickSort(r, a, 0, n-1)
-        fmt.Println("post-quicksort   :", a)
-    }
+
+    array := []int{3, 4, 2, 5, 7, 1, 8, 2, 9, 10}
+    quickSort(rng, array, 0, len(array)-1)
+    fmt.Println("post-quicksort   :", array)
 }
