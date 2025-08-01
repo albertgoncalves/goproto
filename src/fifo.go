b6a67776832c3a25694b0275b23ed6f1c77161b8
albertgoncalves <goncalves.albert@gmail.com>
2025-08-01 17:02:05 -0400

    new, simpler fifo queue

---
+++ src/fifo.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+    "fmt"
+)
+
+type fifo[T any] struct {
+    in  []T
+    out []T
+}
+
+func (self *fifo[T]) push(x T) {
+    self.in = append(self.in, x)
+}
+
+func (self *fifo[T]) pop() T {
+    if len(self.out) == 0 {
+        n := len(self.in)
+        for i := range n {
+            self.out = append(self.out, self.in[n - (i + 1)])
+        }
+        self.in = self.in[:0]
+    }
+
+    n := len(self.out)
+    x := self.out[n - 1]
+    self.out = self.out[:n - 1]
+    return x
+}
+
+func main() {
+    fifo := fifo[int]{}
+
+    for i := range 10 {
+        fifo.push(i)
+    }
+    fmt.Println(fifo)
+
+    for range 5 {
+        fmt.Println(fifo.pop())
+    }
+    fmt.Println(fifo)
+
+    for i := range 5 {
+        fifo.push(i)
+    }
+    fmt.Println(fifo)
+
+    for range 10 {
+        fmt.Println(fifo.pop())
+    }
+    fmt.Println(fifo)
+}
